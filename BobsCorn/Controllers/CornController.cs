dfe388932f4ea77d2f29245aa7a176fda44cc9e7
Jamo <Jamo@W11JAMO>
2026-04-16 16:12:41 -0600

    Add project files.

---
+++ BobsCorn/Controllers/CornController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BobsCorn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CornController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Success");
+        }
+    }
+}
